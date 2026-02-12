afe1ab89656fad6b046bbc01599975752495206e
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-12 22:35:15 +0100

    unify image gen providers

---
+++ src/Aco228.Runners/Helpers/Tasks/TaskHelper.cs
@@ -0,0 +1,15 @@
+﻿using Aco228.Common;
+using Aco228.Runners.Core.Tasks;
+
+namespace Aco228.Runners.Helpers.Tasks;
+
+public static class TaskHelper
+{
+    public static T Get<T>()
+        where T : ITask
+    {
+        var task = ServiceProviderHelper.Construct<T>();
+        return task;
+    }
+    
+}
