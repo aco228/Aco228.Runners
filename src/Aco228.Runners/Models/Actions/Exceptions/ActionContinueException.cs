0379beff6957a50a6ae867b9d95f1fe602ef0b20
Aleksandar Konatar <aleksandar.k03@gmail.com>
2026-02-20 17:55:00 +0100

    import article logic

---
+++ src/Aco228.Runners/Models/Actions/Exceptions/ActionContinueException.cs
@@ -0,0 +1,9 @@
+﻿namespace Aco228.Runners.Models.Actions.Exceptions;
+
+public class ActionContinueException : Exception
+{
+    public ActionContinueException(string message) : base(message)
+    {
+        
+    }
+}
